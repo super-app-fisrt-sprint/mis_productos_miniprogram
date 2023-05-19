dc75b4bf3ad6c6ce7df0c8d5646e63637870f061
andres p <andresfpaternina@gmail.com>
2023-05-19 00:02:28 -0500

    the last commit

--- pages/Packages/Packages.js
+++ pages/Packages/Packages.js
@@ -1,11 +1,21 @@
 Page({
   data: {
+    Doc:"NN",
     name:getApp().globalData.nombre,
     nit:getApp().globalData.DocumentNumber,
     numberAccount:getApp().globalData.NumberAccount,
     categoriaList:[]
   },
   onLoad(e) {
+    if(getApp().globalData.DocumentType==1){
+      this.setData({
+        Doc:"CC",
+      })
+    }else if(getApp().globalData.DocumentType==2){
+      this.setData({
+        Doc:"Nit",
+      })
+    }
     const categoriaList=JSON.parse(e.categoria);
     console.log(categoriaList)
     this.setData({
